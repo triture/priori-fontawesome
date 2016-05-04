f20c84c8260a8719dbfdf021990021d5c1ef9237
Felipe Peternella <felipevex@gmail.com>
2016-05-04 00:29:14 -0300

    Initial release

---
+++ priori/fontawesome/PriFAIcon.hx
@@ -0,0 +1,57 @@
+package priori.fontawesome;
+
+import priori.view.component.PriExtendable;
+
+class PriFAIcon extends PriExtendable {
+
+    @:isVar public var iconColor(default, set):Int;
+    @:isVar public var iconSize(default, set):Int;
+    @:isVar public var icon(default, set):String;
+
+    public function new() {
+        super();
+
+        this.iconColor = 0x000000;
+        this.iconSize = 20;
+        this.icon = "fa-ship";
+    }
+
+    override private function getComponentCode():String {
+        // http://fortawesome.github.io/Font-Awesome/examples/
+        return "<i class=\"fa\"></i>";
+    }
+
+    @:noCompletion private function set_iconColor(value:Int) {
+        this.iconColor = value;
+
+        if (this._baseElement != null) {
+            this._baseElement.css("color", "#" + StringTools.hex(value, 6));
+        }
+
+        return value;
+    }
+
+    @:noCompletion private function set_iconSize(value:Int) {
+        this.iconSize = value;
+
+        if (this._baseElement != null) {
+            this._baseElement.css("font-size", value);
+        }
+
+        return value;
+    }
+
+
+    @:noCompletion private function set_icon(value:String) {
+        this.icon = value;
+
+        if (this._baseElement != null) {
+            this._baseElement.removeClass();
+            this._baseElement.addClass("fa");
+            this._baseElement.addClass(value);
+        }
+
+        return value;
+    }
+
+}
